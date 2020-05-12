af313ffe804fe5dce98e9b68aef5a3984f6f5c7a
gabihoffmann <ah.gabriella@gmail.com>
2020-05-11 23:30:35 -0300

    show the product description

--- huntweb-rocketseat/src/pages/product/index.js
+++ huntweb-rocketseat/src/pages/product/index.js
@@ -1,9 +1,38 @@
 import React from 'react'
 import { Component } from 'react';
+import api from '../../services/api';
+import {Link} from 'react-router-dom'
+
+import './styles.css'
 
 
 export default class Product extends Component{
+    state = {
+        product : {},
+    };
+
+    async componentDidMount(){
+        const {id} = this.props.match.params;
+        const response = await api.get(`/products/${id}`)
+
+        this.setState({product: response.data})
+    }
+
     render(){
-        return <h2>Produto</h2>
+        const {product} = this.state;
+        return (
+            <div className="container">
+                 <div className="product-info">
+                    <h1>{product.title}</h1>
+                    <p>{product.description}</p>
+                    <p>
+                        URL: <a href={product.url}>{product.url}</a>
+                    </p>
+                </div>
+               <div className="actions">
+                    <button><Link to='/'>Voltar</Link></button>
+               </div>
+            </div>
+        )
     }
 }
