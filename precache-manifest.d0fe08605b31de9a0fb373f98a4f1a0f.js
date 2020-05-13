e2033f5cdaf64c681ac698153c9d72dcca83dfbb
gabihoffmann <ah.gabriella@gmail.com>
2020-05-13 16:39:47 -0300

    Updates

---
+++ precache-manifest.d0fe08605b31de9a0fb373f98a4f1a0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f34d7137941ccf7e08852df5d2642352",
+    "url": "/gabihoffmann/huntweb-rocketseat/index.html"
+  },
+  {
+    "revision": "68fa82dcb44097fe6b73",
+    "url": "/gabihoffmann/huntweb-rocketseat/static/css/main.d0447c0b.chunk.css"
+  },
+  {
+    "revision": "3142effeacd6d0938992",
+    "url": "/gabihoffmann/huntweb-rocketseat/static/js/2.d44a9a2c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/gabihoffmann/huntweb-rocketseat/static/js/2.d44a9a2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68fa82dcb44097fe6b73",
+    "url": "/gabihoffmann/huntweb-rocketseat/static/js/main.cd09ead2.chunk.js"
+  },
+  {
+    "revision": "6913dacb1a44d077137a",
+    "url": "/gabihoffmann/huntweb-rocketseat/static/js/runtime-main.6e82039f.js"
+  }
+]);
